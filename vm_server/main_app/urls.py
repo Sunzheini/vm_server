9490c8dfa06750fb9fd6e6bd9d7440992787b288
Sunzheini <daniel_zorov@abv.bg>
2023-08-24 17:08:29 +0300

    added api functionalities

--- vm_server/main_app/urls.py
+++ vm_server/main_app/urls.py
@@ -1,9 +1,27 @@
 from django.urls import path
 
-from vm_server.main_app.views import index, test
-
+from vm_server.main_app.views import index, test, get_data, add_item, get_item, delete_item, update_item
 
 urlpatterns = [
     path('', index, name='index'),
-    path('/test', test, name='test'),
+    path('test/', test, name='test'),
+
+    # http://127.0.0.1:8000/api/
+    path('api/', get_data, name='get data'),
+
+    # http://127.0.0.1:8000/api/get/2/
+    # http://127.0.0.1:8000/api/get/Item2/
+    path('api/get/<str:identifier>/', get_item, name='get item'),
+
+    # http://127.0.0.1:8000/api/add/
+    path('api/add/', add_item, name='add item'),
+
+    # http://127.0.0.1:8000/api/delete/1/
+    # http://127.0.0.1:8000/api/get/2/
+    # http://127.0.0.1:8000/api/get/Item2/
+    path('api/delete/<str:identifier>/', delete_item, name='delete_item'),
+
+    # http://127.0.0.1:8000/api/update/2/
+    # http://127.0.0.1:8000/api/update/Item2/
+    path('api/update/<int:identifier>/', update_item, name='update_item'),
 ]
--- vm_server/main_app/views.py
+++ vm_server/main_app/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from vm_server.main_app.models import Item
+from vm_server.main_app.serializers import ItemSerializer
 
 
 """
@@ -12,3 +17,76 @@ def index(request):
 
 def test(request):
     return render(request, 'main_app/test.html')
+
+
+@api_view(['GET'])
+def get_data(request):
+    all_objects = Item.objects.all()
+    serializer = ItemSerializer(
+        all_objects,
+        many=True,      # if many=True, if only 1=False
+    )
+    return Response(
+        serializer.data,    # we want the .data
+    )
+
+
+@api_view(['GET'])
+def get_item(request, identifier):
+    try:
+        # First, try to get by ID (numeric identifier)
+        item = Item.objects.get(pk=identifier)
+    except (Item.DoesNotExist, ValueError):
+        # If not found or not numeric, try to get by name
+        try:
+            item = Item.objects.get(name=identifier)
+        except Item.DoesNotExist:
+            return Response({'error': 'Item not found'}, status=404)
+
+    serializer = ItemSerializer(item)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def add_item(request):
+    serializer = ItemSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+# post like this:
+"""
+{
+"name":"Item from post"
+}
+"""
+
+
+@api_view(['DELETE'])
+def delete_item(request, identifier):
+    try:
+        # First, try to delete by ID (numeric identifier)
+        item = Item.objects.get(pk=identifier)
+    except (Item.DoesNotExist, ValueError):
+        # If not found or not numeric, try to delete by name
+        try:
+            item = Item.objects.get(name=identifier)
+        except Item.DoesNotExist:
+            return Response({'error': 'Item not found'}, status=404)
+
+    item.delete()
+    return Response({'message': 'Item deleted successfully'})
+
+
+@api_view(['PUT', 'PATCH'])
+def update_item(request, identifier):
+    try:
+        item = Item.objects.get(pk=identifier)
+    except Item.DoesNotExist:
+        return Response({'error': 'Item not found'}, status=404)
+
+    serializer = ItemSerializer(item, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
